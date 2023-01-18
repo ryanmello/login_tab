725111320bda192b400a72a11533277175411c06
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-01-18 17:17:01 +0000

    revamp

--- Main.java
+++ Main.java
@@ -15,8 +15,9 @@ public class Main implements ActionListener{
   private static JLabel successLabel;
   
   public static void main(String[] args) {
+
     frame = new JFrame();
-    frame.setSize(500, 500);
+    frame.setSize(400, 400);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     panel = new JPanel();
@@ -25,42 +26,64 @@ public class Main implements ActionListener{
     userLabel = new JLabel("User");
     userLabel.setBounds(10, 20, 80, 25);
 
-    userText = new JTextField(20);
+    userText = new JTextField();
     userText.setBounds(100, 20, 165, 25);
 
     passwordLabel = new JLabel("Password");
-    passwordLabel.setBounds(10, 50, 80, 25);
+    passwordLabel.setBounds(10, 60, 80, 25);
 
-    passwordText = new JPasswordField(20);
-    passwordText.setBounds(100, 50, 165, 25);
+    passwordText = new JPasswordField();
+    passwordText.setBounds(100, 60, 165, 25);
     
     button = new JButton("Login");
     button.addActionListener(new Main());
-    button.setBounds(10, 80, 80, 25);
+    button.setBounds(10, 100, 80, 25);
 
-    successLabel = new JLabel("");
-    successLabel.setBounds(10, 110, 300, 25);
+    successLabel = new JLabel();
+    successLabel.setBounds(10, 130, 80, 25);
     successLabel.setForeground(Color.green);
+    //successLabel.setText("Success!");
     
-    panel.add(successLabel);
-    panel.add(button);
-    panel.add(userText);
-    panel.add(userLabel);
-    panel.add(passwordText);
-    panel.add(passwordLabel);
+
+    frame.add(successLabel);
+    frame.add(button);
+    frame.add(passwordText);
+    frame.add(passwordLabel);
+    frame.add(userText);
+    frame.add(userLabel);
     frame.add(panel);
     frame.setVisible(true);
+
+    
+  
   }
 
   public void actionPerformed(ActionEvent e){
     String user = userText.getText();
     String password = passwordText.getText();
-    // if(userText.getText() == "ryan" && passwordLabel.getText() == "123"){
-    //   //successLabel.setText("Success!");
-    //   System.out.println("Success");
-    // }
+
     if(user.equals("ryan") && password.equals("123")){
-      successLabel.setText("Login Successful!");
+      successLabel.setText("Sucess!");
     }
+    
   }
+
+
+
+
+
+
+
+  
+  
+  // public void actionPerformed(ActionEvent e){
+  //   String user = userText.getText();
+  //   String password = passwordText.getText();
+  //   // if(userText.getText() == "ryan" && passwordLabel.getText() == "123"){
+  //   //   //successLabel.setText("Success!");
+  //   //   System.out.println("Success");
+  //   // }
+  //   if(user.equals("ryan") && password.equals("123")){
+  //     successLabel.setText("Login Successful!");
+  //   }
 }
